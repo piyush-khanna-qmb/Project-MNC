641f06ac0be32ac9fd089532ed2316ab3617e63a
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-07-18 18:33:27 +0530

    121

---
+++ 1.two-sum.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+
+import java.util.HashMap;
+
+class Solution 
+{
+    public int[] twoSum(int[] nums, int target) 
+    {
+        HashMap<Integer, Integer> hm= new HashMap();
+        int n= nums.length, k;
+        for(int i= 0; i<n; i++)
+        {
+            k= target - nums[i];
+            if(hm.containsKey(k)) {
+                return new int[] {i, hm.get(k)};
+            }
+            hm.put(nums[i], i);
+        }
+        return new int[] {0,0};
+    }
+}
+// @lc code=end
+
---
+++ 121.best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=121 lang=java
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+class Solution 
+{
+    public int maxProfit(int[] prices) 
+    {
+        
+        return 0;
+    }
+}
+// @lc code=end
