c9444499631da181f0391d61119f129be20a9ccf
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-07-18 18:52:56 +0530

    121 2

--- 121.best-time-to-buy-and-sell-stock.java
+++ 121.best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,16 @@
-/*
- * @lc app=leetcode id=121 lang=java
- *
- * [121] Best Time to Buy and Sell Stock
- */
-
-// @lc code=start
 class Solution 
 {
-    public int maxProfit(int[] prices) 
+    public int maxProfit(int[] a) 
     {
-        
-        return 0;
+        int grPro= 0, n= a.length, m= Integer.MAX_VALUE, pro= 0;
+        for(int i=0; i<n; i++)
+        {
+            if(a[i] < m)
+                m= a[i];
+            pro= a[i]-m;
+            if(grPro < pro)
+                grPro= pro;
+        }
+        return grPro;
     }
 }
-// @lc code=end
