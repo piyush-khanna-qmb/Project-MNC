194c4db84a1d515994aae9b095e06abd69cb41cc
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-07-28 17:18:36 +0530

    28

--- 28.find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.java
@@ -9,7 +9,20 @@ class Solution
 {
     public int strStr(String haystack, String needle) 
     {
-        return haystack.indexOf(needle);
+        int i, j= 0;
+        for(i=0; i<haystack.length(); i++)
+        {
+            if(haystack.charAt(i)==needle.charAt(j))
+                j++;
+            else
+                j=0;
+            if(j==needle.length())
+            {
+                i= i-j+1;
+                return i;
+            }
+        }
+        return -1;
     }
 }
 // @lc code=end
