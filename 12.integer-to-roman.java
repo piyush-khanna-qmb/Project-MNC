a7497eeb87c05bf6ae3654c35ef1aa23a74b3049
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-08-02 21:55:38 +0530

    12 mast

--- 12.integer-to-roman.java
+++ 12.integer-to-roman.java
@@ -9,7 +9,20 @@ class Solution
 {
     public String intToRoman(int num) 
     {
-        
+        String roms[]= {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+        int nums[]= {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+
+        StringBuilder sb= new StringBuilder();
+        int i= nums.length()-1;
+
+        while(num>0)
+        {
+            if(num > nums[i])
+                sb.append(roms[i])
+            else
+                i--;
+        }
+        return new String(sb);
     }
 }
 // @lc code=end
