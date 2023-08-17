7d9c095e910f82605c5b2d7abc4ae023dc11ed9c
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-08-18 00:06:53 +0530

    zigzag shuru

---
+++ 6.zigzag-conversion.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=6 lang=java
+ *
+ * [6] Zigzag Conversion
+ */
+
+// @lc code=start
+
+class Solution 
+{
+    public String convert(String s, int numRows) 
+    {
+        
+    }
+}
+// @lc code=end
+
