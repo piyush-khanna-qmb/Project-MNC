564114e2b99b3b3e85613f44b06b533641321e5d
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-08-18 22:41:31 +0530

    Fastest bitch out there

---
+++ 535.encode-and-decode-tiny-url.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=535 lang=java
+ *
+ * [535] Encode and Decode TinyURL
+ */
+
+// @lc code=start
+public class Codec 
+{
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) 
+    {
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) 
+    {
+        return shortUrl;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
+// @lc code=end
+
