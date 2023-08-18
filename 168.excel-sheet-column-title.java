f12459e04d61268a20efabe28ec78ea98c1c80d9
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-08-18 21:14:20 +0530

    168

--- 168.excel-sheet-column-title.java
+++ 168.excel-sheet-column-title.java
@@ -9,7 +9,14 @@ class Solution
 {
     public String convertToTitle(int columnNumber) 
     {
-            
+        StringBuilder sb= new StringBuilder();
+        int quo, rem;
+        do {
+            rem= --columnNumber%26;
+            sb.insert(0, (char)(rem+'A'));
+            columnNumber= columnNumber/26;
+        } while(columnNumber != 0);
+        return sb.toString();
     }
 }
 // @lc code=end
