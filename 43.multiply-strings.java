72b29e165d937e858c0b5f6919f91ddb58eef566
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-08-18 23:37:30 +0530

    multiply strings ki mkc

---
+++ 43.multiply-strings.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=43 lang=java
+ *
+ * [43] Multiply Strings
+ */
+
+// @lc code=start
+class Solution 
+{
+    public String multiply(String num1, String num2) 
+    {
+        if(num1.equals("0") || num2.equals("0"))
+            return "0";
+        
+        int cn, cu;
+        for(int i= num2.length()-1; i>=0; i--)
+        {
+            cn= num2.charAt(i) - '0';
+            
+            for()
+        }
+    }
+}
+// @lc code=end
+
