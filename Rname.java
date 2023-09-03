4f1a282bb0cf679229053c2ec51effe6a7bb07c8
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-09-03 23:22:47 +0530

    rname

--- Rname.java
+++ Rname.java
@@ -1,26 +1,24 @@
 import java.util.*;
 class RName
 {
-    static int fun(int i, int arr[], ArrayList<Integer> ls, int ksum)
+    static int fun(int i, int arr[], int s, int ksum)
     {
-        if(i == arr.length) {
-            int sum= 0;
-            for(int s: ls)
-                sum+= s;
-            if(sum==ksum) 
+        if (i == arr.length) {
+            if(s==ksum) {
                 return 1;
+            }
             return 0;
         }
-        ls.add(arr[i]);
-        int addKrke= fun(i+1, arr, ls, ksum);
-        ls.remove(Integer.valueOf(arr[i]));
-        int binaAddKrke= fun(i+1, arr, ls, ksum);
-        return addKrke+binaAddKrke;
+        s+= arr[i];
+        int left= fun(i+1, arr, s, ksum);
+        s-= arr[i];
+        int right= fun(i+1, arr, s, ksum);
+
+        return left+right;
     }
     public static void main(String args[])
     {
         int arr[]= {2,1,2,1}; 
-        ArrayList<Integer> ls= new ArrayList();
-        System.out.println(fun(0, arr, ls, 3));
+        System.out.println(fun(0, arr, 0, 3));
     }
 }
