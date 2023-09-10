11044a1fc6430c0e706da1e4c9b2f2eb63db1176
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-09-10 16:03:41 +0530

    tatti 9

--- 39.combination-sum.java
+++ 39.combination-sum.java
@@ -5,28 +5,33 @@
  */
 
 // @lc code=start
-class Solution {
-
-    private void nikaaliyo(List<List<Integer>> ans, List<Integer> beech, int arr[], int tar, int i, int s) {
-
-        if(i == arr.length) {
-            if(s==tar) {
-                ans.add(beech);
-            }
+class Solution 
+{
+    private void nikaaliyo(List<List<Integer>> ans, List<Integer> beech, int arr[], int tar, int i, int s) 
+    {
+        if(s==tar) {
+            ans.add(beech);
+        }
+        if(s>tar) {   
+            return;
         }
         
+        //  Yahi
         s+= arr[i];
         beech.add(arr[i]);
-        nikaaliyo(ans, beech, arr, tar, i+1, s);
+        nikaaliyo(ans, beech, arr, tar, i, s);
 
+        // Agla
         s-= arr[i];
-        beech.remove(Integer.valueOf(arr[i]));
-        nikaaliyo(ans, beech, arr, tar, i+1, s);
+        beech.remove(beech.size()-1);
+        nikaaliyo(ans, beech, arr, tar, i, s);
     }
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) 
+    {
         List<List<Integer>> ans= new ArrayList();
-        return nikaaliyo(ans, new ArrayList(), candidates, target, 0, 0);
+        nikaaliyo(ans, new ArrayList(), candidates, target, 0, 0);
+        return ans;
     }
 }
 // @lc code=end
