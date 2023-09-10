909432a446e0429b25b4b0f7bb9b6715b0af5a4a
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-09-10 14:29:43 +0530

    qs

--- quickSort.java
+++ quickSort.java
@@ -1,13 +1,11 @@
 import java.util.*;
 class quickSort {
-
     private static int partition(int arr[], int low, int high) {
-        int pivot= arr[low];
-        int i= low, j= high, tmp;
+        int p= arr[low], i= low, j= high, tmp;
         while(i<j) {
-            while(arr[i]<=pivot && i<high) 
+            while(i<high && arr[i]<=p)
                 i++;
-            while(arr[j]>pivot && j>low)
+            while(j>low && arr[j]>p)
                 j--;
             if(i<j) {
                 tmp= arr[i];
@@ -20,17 +18,15 @@ class quickSort {
         arr[j]= tmp;
         return j;
     }
-
     private static void qS(int arr[], int low, int high) {
         if(low>high)
             return;
         int part= partition(arr, low, high);
         qS(arr, low, part-1);
         qS(arr, part+1, high);
-    }
-
+    }    
     public static void main(String args[]) {
-        int a[]= {12,46,897,2,43,87,2,1,546,9,04,5};
+        int a[]= {12,65,3,7,98,1,65,8,3,2};
         qS(a, 0, a.length-1);
         System.out.println(Arrays.toString(a));
     }
