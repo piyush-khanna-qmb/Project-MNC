2e340868aca255965bd46a518bf822639bcfe9bf
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-09-15 18:45:20 +0530

    power ham bhi rakhte hai pehewaane

--- 50.pow-x-n.java
+++ 50.pow-x-n.java
@@ -9,9 +9,20 @@ class Solution
 {
     public double myPow(double x, int n) 
     {
-        if(n==0)
+        if (n==1)
             return x;
-        return x*myPow(x, n--);
+        
+        if(n<0)
+        {
+            x= 1/x;
+            n= -n;
+        }
+
+        double halfPow= myPow(x, n/2);
+        if(n%2 == 0)
+            return halfPow * halfPow; 
+        else
+            return x * halfPow * halfPow;
     }
 }
 // @lc code=end
