e9fe7174664767ffe3ea69e6c1de9903e29a91a3
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-09-20 02:23:54 +0530

    huihuihui

--- 47.permutations-ii.java
+++ 47.permutations-ii.java
@@ -7,27 +7,22 @@
 // @lc code=start
 class Solution 
 {
-    private void func(List<List<Integer>> ans, List<Integer> ds, int arr[], boolean vis[])
+    private void func(List<List<Integer>> ans, List<Integer> ds, int nums[], boolean vis[])
     {
-        if(ds.size() == arr.length)
+        if(ds.size() == nums.length)
         {
             ans.add(new ArrayList(ds));
             return;
         }
-        for(int i=0; i<arr.length; i++)
-        {
-            if(vis[i] || (i>0 && arr[i]==arr[i-1] && !vis[i-1]))
-                /*
-                i>0 sirf isliye hai kyuki arr[i-1] krne me underflow na ho jaye, although main motive hai ki koi value last value ke baraabar na ho tatha usse pichhle wala visited na ho to vo case usi recursion ka hissa hoga jo redundant hogi, isliye use chhodh do.
 
-                In input example: [1,1,1,2]
-                Ye || ke baad waala saara jhamela isliye likha gaya hai taaki [❌,✅,❌,❌] wali condition na ban paaye. Bole to aisi value jo mujhe pta hai ki pehle hi tackle kar li gyi hogi kyuki usse pehle same hi value aati hai uske lena hi nhi hai, check bhi nhi krna ki usse kuchh naya permute ban skta hai ki nhi, kyuki ban hi nahi sakta.
-                */
+        for(int i= 0; i<nums.length; i++)
+        {
+            if(vis[i] || (i>0 && nums[i]==nums[i-1] && !vis[i-1]))
                 continue;
             
             vis[i]= true;
-            ds.add(arr[i]);
-            func(ans, ds, arr, vis);
+            ds.add(nums[i]);
+            func(ans, ds, nums, vis);
             vis[i]= false;
             ds.remove(ds.size()-1);
         }
@@ -39,6 +34,7 @@ class Solution
         List<Integer> ds = new ArrayList();
         boolean vis[]= new boolean[nums.length];
         Arrays.sort(nums);
+
         func(ans, ds, nums, vis);
         return ans;
     }
