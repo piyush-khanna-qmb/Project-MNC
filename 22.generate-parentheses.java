7b332383bec8fd6f072e9c9b406063bffd225ce2
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-09-21 03:05:01 +0530

    dekhi jayegi

--- 22.generate-parentheses.java
+++ 22.generate-parentheses.java
@@ -7,9 +7,61 @@
 // @lc code=start
 class Solution 
 {
+    private boolean isValid(String s)
+    {
+        Stack<Character> stack= new Stack();
+        for(char ch: s.toCharArray())
+        {
+            if(ch=='(')
+                stack.push(ch);
+            else
+            {
+                if(!stack.isEmpty())
+                    stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    private void permutes(List<String> sab, StringBuilder ds, StringBuilder sb, boolean vis[])
+    {
+        if(ds.length() == sb.length())
+        {
+            sab.add(new String(ds));
+            return;
+        }
+        for(int j= 0; j<sb.length(); j++)
+        {
+            if(vis[j] || (j>0 && sb.charAt(j)==sb.charAt(j-1) && !vis[j-1]))
+                continue;
+
+            vis[j]= true;
+            ds.append(sb.charAt(j));
+            permutes(sab, ds, sb, vis);
+
+            vis[j]= false;
+            ds.deleteCharAt(ds.length()-1);
+        }
+    }
+
     public List<String> generateParenthesis(int n)
     {
-        return new ArrayList<String>();
+        StringBuilder sb= new StringBuilder();
+        for(int i= 0; i<n; i++)
+            sb.append("(");
+        for(int i= 0; i<n; i++)
+            sb.append(")");
+
+        boolean vis[]= new boolean[n*2];
+        List<String> sab= new ArrayList();
+        permutes(sab, new StringBuilder(), sb, vis);
+        List<String> ans= new ArrayList();
+        for(String kk: sab)
+        {
+            if(isValid(kk))
+                ans.add(kk);
+        }
+        return ans;
     }
 }
 // @lc code=end
