cb328a64bd1a7b7d39140011858af0a19d0142ef
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-09-25 01:14:07 +0530

    1025 initiated 🫡

---
+++ 1025.divisor-game.java
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1025 lang=java
+ *
+ * [1025] Divisor Game
+ */
+
+// @lc code=start
+class Solution 
+{
+    public boolean divisorGame(int n) 
+    {
+        
+    }
+}
+// @lc code=end
+
