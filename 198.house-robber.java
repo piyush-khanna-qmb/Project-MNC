2024be2aab9ea12c27eb897f52f61401ac6de322
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-09-29 03:53:59 +0530

    tabul

--- 198.house-robber.java
+++ 198.house-robber.java
@@ -7,28 +7,20 @@
 // @lc code=start
 class Solution 
 {
-    private int func(int nums[], int i, int dp[])
-    {
-        if(i < 0)
-            return 0;
-        else if(i == 0)
-            return nums[0];
-
-        if(dp[i]!=-1)
-            return dp[i];
-        
-        int pick= nums[i] + func(nums, i-2, dp);
-        int noPick= func(nums, i-1, dp);
-
-        return dp[i]= Math.max(pick, noPick);
-    }
     public int rob(int[] nums) 
     {
-        int dp[]= new int[nums.length];
-        for(int i=0; i<nums.length; i++) {
-            dp[i]= -1;
+        int a= 0;
+        int b= nums[0];
+
+        for(int i= 1; i<nums.length; i++)
+        {
+            int pick= nums[i]+a;
+            int noPick= b;
+            nums[i]= Math.max(pick, noPick);
+            a=b;
+            b=nums[i];
         }
-        return func(nums, nums.length-1, dp);
+        return b;
     }
 }
 // @lc code=end
