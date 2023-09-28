45a65f80fe5d55983f0eadf12f1eed4511cc1033
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-09-29 02:32:03 +0530

    chal gaya betiKL

--- frogJumpWithK.java
+++ frogJumpWithK.java
@@ -1,14 +1,14 @@
 class frogJumpWithK
 {
-    private static int doShit(int arr[], int k, int n, int i)
+    private static int doShit(int arr[], int k, int i)
     {
         if(i<=0)
             return 0;
 
         int minCost= Integer.MAX_VALUE;
-        for(int t= 1; i<=k; t++)
+        for(int t= 1; t<=k && i-t>=0; t++)
         {
-            int cost= Math.abs(arr[i]-arr[t]) + doShit(arr, k, n, i-t);
+            int cost= Math.abs(arr[i]-arr[i-t]) + doShit(arr, k, i-t);
             minCost= Math.min(cost, minCost);
         }
         return minCost;
@@ -18,6 +18,6 @@ class frogJumpWithK
     {
         int arr[]= {40, 10, 20, 70, 80, 10, 20, 70, 80, 60};
         int k= 4;
-        System.out.println( doShit(arr, k, arr.length, arr.length-1) );
+        System.out.println( doShit(arr, k, arr.length-1) );
     }
 }
