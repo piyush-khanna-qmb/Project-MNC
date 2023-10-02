70585980eda583db8b767493014b010a08f75ddd
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-03 01:09:05 +0530

    chori kiya re jiya

--- 213.house-robber-ii.java
+++ 213.house-robber-ii.java
@@ -7,33 +7,32 @@
 // @lc code=start
 class Solution 
 {
-    public int func(int nums[], int i, boolean noFir, int dp[])
+    public int chorBKL(int nums[], int i, int dp[])
     {
         if(i < 0)
             return 0;
-        else if(i==0)
-        {
-            if(noFir)
-                return 0;
-            return nums[0];
-        }
+        if(i == 0)
+            return dp[i]= nums[i];
+
         if(dp[i]!=-1)
             return dp[i];
-        int pick= nums[i] + func(nums, i-2, noFir, dp);
-        int noPick= func(nums, i-1, noFir, dp);
+
+        int pick= nums[i] + chorBKL(nums, i-2, dp);
+        int noPick= chorBKL(nums, i-1, dp);
 
         return dp[i]= Math.max(pick, noPick);
     }
     public int rob(int[] nums) 
     {
-        if(nums.length==1)
-            return nums[0];
         int n= nums.length;
+        if(n==1)
+            return nums[0];
         int dp[]= new int[n];
-        for(int i=0; i<n; i++)
-            dp[i]= -1;
-        int noFirst= func(nums, n-1, true, dp);
-        int noLast= func(nums, n-2, false, dp);
+        Arrays.fill(dp, -1);
+
+        int noFirst= chorBKL(Arrays.copyOfRange(nums, 1, n), n-2, dp);  // Yahaan n-1 ki jagah n-2 aayega kyuki length hamne ek se kam kar di hai
+        Arrays.fill(dp, -1);
+        int noLast= chorBKL(nums, n-2, dp);
         return Math.max(noFirst, noLast);
     }
 }
