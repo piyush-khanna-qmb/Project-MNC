80d295764ba4e74be8d04063164f3e9431ff64b2
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-03 02:43:09 +0530

    Amara cheetah ninja

--- ninTraining.java
+++ ninTraining.java
@@ -1,6 +1,7 @@
-public class ninTraining 
+if(dp[n][last] != -1)
+            return dp[n][last];public class Solution
 {
-    private static int f(int n, int points[][], int last)
+    private static int f(int n, int points[][], int last, int dp[][])
     {
         if(n == 0)
         {
@@ -12,18 +13,28 @@ public class ninTraining
             }
             return maxi;
         }
+        if(dp[n][last] != -1)
+            return dp[n][last];
 
         int maxi= 0;
         for(int i=0; i<3; i++)
         {
             if(i != last)
-                maxi= Math.max(maxi, points[n][i]+f(n, points, i));
+                maxi= Math.max(maxi, points[n][i]+f(n-1, points, i, dp));
         }
-        return maxi;
+        return dp[n][last]= maxi;
     }
 
     public static int ninjaTraining(int n, int points[][]) 
     {
-        return f(n, points, 3);
+        int dp[][]= new int[n][4];
+        for(int i= 0; i<n; i++)
+        {
+            for(int j= 0; j<4; j++)
+            {
+                dp[i][j]= -1;
+            }
+        }
+        return f(n-1, points, 3, dp);
     }
 }
