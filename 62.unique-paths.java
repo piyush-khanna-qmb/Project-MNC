8125cb909bd032c62e7748de56b21553ec02827f
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-08 02:11:33 +0530

    single dp bc!

--- 62.unique-paths.java
+++ 62.unique-paths.java
@@ -7,27 +7,25 @@
 // @lc code=start
 class Solution 
 {
-    private int safar(int m, int n, int dp[][])
-    {
-        if(m==0 && n==0)
-            return 1;
-        if(m<0 || n<0)
-            return 0;
-        
-        if(dp[m][n] != -1)
-            return dp[m][n];
-
-        int left= safar(m-1, n, dp);
-        int up= safar(m, n-1, dp);
-        return dp[m][n]= left+up;
-    }
     public int uniquePaths(int m, int n) 
     {
-        int dp[][]= new int[m][n];
-        for(int r[]: dp)
-            Arrays.fill(r, -1);
-        return safar(m-1, n-1, dp);
+        int prev[]= new int[n];
+        int temp[]= Arrays.copyOf(prev, n);
+        temp[0]= 1;
+        int up, left;
+        for(int i= 0; i<m; i++)
+        {
+            for(int j= 0; j<n; j++)
+            {
+                if(i==0 && j==0)
+                    continue;
+                left= (j>0) ? temp[j-1] : 0;
+                up= (i>0) ? prev[j] : 0;
+                temp[j]= left+up;
+            }
+            prev= temp;
+        }
+        return prev[n-1];
     }
 }
 // @lc code=end
-
