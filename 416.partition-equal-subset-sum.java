8284e002c0ce089b0cd367397af1777e3f897ccf
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-09 02:39:41 +0530

    Beerpaal

---
+++ 416.partition-equal-subset-sum.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=416 lang=java
+ *
+ * [416] Partition Equal Subset Sum
+ */
+
+// @lc code=start
+class Solution 
+{
+    private int f(int nums[], int i, int tar, int dp[][])
+    {
+        if(tar == 0)
+            return 1;
+        if(i<0 || tar<0)
+            return 0;
+        if(dp[i][tar]!=-1)
+            return dp[i][tar];
+
+        int take= f(nums, i-1, tar-nums[i], dp);
+        if(take == 1)
+            return 1;
+        return  f(nums, i-1, tar, dp);
+    }
+
+    public boolean canPartition(int[] nums) 
+    {
+        int sum= 0;
+        for(int n: nums)
+            sum+= n;
+        
+        if(sum%2 == 1)
+            return false;
+        
+        int n= nums.length;
+        boolean dp[][]= new boolean[n+1][sum/2 + 1];
+        
+        for(int i= 0; i<n; i++)
+            dp[i][0]= true;
+        
+        for(int i= 1; i<n; i++)
+        {
+            for(int tar= 1; tar<=sum/2; tar++)
+            {
+                boolean take= false;
+                if(tar>=nums[i])
+                    take= dp[i-1][tar-nums[i]];
+                boolean notTake= dp[i-1][tar];
+                dp[i][tar]= take||notTake;
+            }
+        }
+        return dp[n-1][sum/2-1];
+    }
+}
+// @lc code=end
