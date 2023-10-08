469cb22f6a31e8c8e91447d163a1d0caf4b6ad48
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-08 23:36:26 +0530

    Duran duran falling down

--- 931.minimum-falling-path-sum.java
+++ 931.minimum-falling-path-sum.java
@@ -11,25 +11,27 @@ class Solution
     {
         int m= matrix.length;
         int n= matrix[0].length;
-        int dp[][]= new int[m][n];
+        int dp[]= new int[n];
+        int prev[]= new int[n];
         for(int i= 0; i<n; i++)
-            dp[0][i]= matrix[0][i];
+            prev[i]= matrix[0][i];
         
-        int ans= Integer.MAX_VALUE;
         for(int r= 1; r<m; r++)
         {
             for(int c= 0; c<n; c++)
             {
-                int up= dp[r-1][c];
-                int lu= c>0? dp[r-1][c-1] : Integer.MAX_VALUE;
-                int ru= c<n-1? dp[r-1][c+1] : Integer.MAX_VALUE;
-                dp[r][c]= matrix[r][c]+Math.min(Math.min(up,lu),ru);
+                int up= prev[c];
+                int lu= (c>0)? prev[c-1] : Integer.MAX_VALUE;
+                int ru= (c<n-1)? prev[c+1] : Integer.MAX_VALUE;
+                dp[c]= matrix[r][c]+Math.min(Math.min(up,lu),ru);
             }
+            prev= Arrays.copyOf(dp, n);
         }
+
+        int ans= Integer.MAX_VALUE;
         for(int i= 0; i<n; i++)
-            ans= Math.min(ans, dp[m-1][i]);
+            ans= Math.min(ans, prev[i]);
         return ans;
     }
 }
 // @lc code=end
-
