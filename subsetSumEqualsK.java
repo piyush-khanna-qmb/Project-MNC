8e0049475e2fba71e2b30c55fadca8125a48fea6
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-09 11:53:37 +0530

    practice

--- subsetSumEqualsK.java
+++ subsetSumEqualsK.java
@@ -2,22 +2,17 @@ import java.util.* ;
 import java.io.*; 
 public class Solution 
 {
-    private static boolean f(int arr[], int tar, int i, int dp[][])
+    private static int f(int arr[], int tar, int i, int dp[][])
     {
-        if(tar == 0)
-            return true;
-        if(i<0)
-            return false;
-        if(dp[i][tar]!= -1)
-            return dp[i][tar] == 1
+        if(tar == 0)    return 1;
+        if(i<0)     return 0;
+        if(dp[i][tar]!=-1)      return dp[i][tar];
 
-        boolean take= arr[i] + f(arr, tar-arr[i], i-1, dp);
-        boolean noTake= false;
-        if(!take)
-            noTake= f(arr, tar, i-1, dp);
-        
-        dp[i][tar]= (take || noTake) ? 1 : 0;
-        return take || noTake;
+        int take= f(arr, tar-arr[i], i-1, dp);
+        if(take == 0)
+            return f(arr, tar, i-1, dp)==1;
+        else
+            return 1;
     }
 
     public static boolean subsetSumToK(int n, int k, int arr[])
@@ -25,6 +20,6 @@ public class Solution
         int dp[][]= new int[n][k];
         for(int r[]: dp)
             Arrays.fill(r, -1);
-        return f(arr, k, n-1, dp);
+        return f(arr, k, n-1, dp)==1;
     }
 }
