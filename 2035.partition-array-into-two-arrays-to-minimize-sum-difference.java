aee00762bbf2316f3a85cba9c3ccf304c9a0f121
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-11 03:00:02 +0530

    Bacche teri mkc!

--- 2035.partition-array-into-two-arrays-to-minimize-sum-difference.java
+++ 2035.partition-array-into-two-arrays-to-minimize-sum-difference.java
@@ -9,39 +9,30 @@ class Solution
 {
     public int minimumDifference(int[] nums) 
     {
-        int n= nums.length;
-        if(n == 2)
-            return Math.abs(nums[0]-nums[1]);
-        
-        int totSum= 0; 
-        for(int ni: nums)
-            totSum+= ni;
-        
-        boolean dp[][]= new boolean[n+1][totSum+1];
-        for(int i= 0; i<n; i++)
-            dp[i][0]= true;
-        
-        for(int i= 1; i<n; i++)
+        int totSum= Arrays.stream(nums).sum();
+        int target= totSum/2;
+
+        boolean dp[]= new boolean[target+1];
+        dp[0]= true;
+
+        for(int num: nums)
         {
-            for(int tar= 1; tar<=totSum; tar++)
+            for(int i= target; i>=num; i--)
             {
-                boolean take= false;
-                if(tar>=nums[i])
-                    take= dp[i-1][tar-nums[i]];
-                boolean noTake= dp[i-1][tar];
-                dp[i][tar]= take || noTake;
+                dp[i]= dp[i] || dp[i-num];
             }
         }
 
-        int mini= Integer.MAX_VALUE;
-        for(int i= 0; i<=(totSum/2); i++)
+        int maax= 0;
+        for(int i= target; i>=0; i--)
         {
-            if(dp[n-1][i]) {
-                mini= Math.min(mini, Math.abs(i-(totSum-i)));
+            if(dp[i])
+            {
+                maax= i;
+                break;
             }
         }
-        return mini;
+        return Math.abs(totSum - 2*maax);
     }
 }
-// @lc code=end
 
