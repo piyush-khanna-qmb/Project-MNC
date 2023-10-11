b7736907a5e5a1d2fca723affd479ed3a45d03d3
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-12 03:32:41 +0530

    with dp

--- countSubsetsWithSum.java
+++ countSubsetsWithSum.java
@@ -1,22 +1,28 @@
 import java.util.*;
 import java.io.*;
 
-public class countSubsetsWithSum
+public class Solution
 {
-    private static int f(int arr[], int tar, int i)
+    private static int f(int arr[], int tar, int i, int dp[][])
     {
+        int mod= (int) Math.pow(10,9)+7;
         if(tar == 0)
             return 1;
         if(i<0)
             return 0;
         
-        int take= f(arr, tar-arr[i], i-1);
-        int notTake= f(arr, tar, i-1);
+        if(dp[i][tar]!=-1)
+            return dp[i][tar];
+        int take= (tar>=arr[i]) ? f(arr, tar-arr[i], i-1, dp)%mod : 0;
+        int notTake= f(arr, tar, i-1, dp)%mod;
 
-        return take+notTake;
+        return dp[i][tar]= take+notTake;
     }
     public static int findWays(int num[], int tar) 
     {
-        return f(num, tar, 0);
+        int dp[][]= new int[num.length][tar+1];
+        for(int n[]: dp)
+            Arrays.fill(n, -1);
+        return f(num, tar, num.length-1, dp);
     }
 }
