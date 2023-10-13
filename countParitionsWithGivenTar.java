a1dd1e8c3b4dc6e903575f2716f7346dc9af516f
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-13 06:24:59 +0530

    boss

--- countParitionsWithGivenTar.java
+++ countParitionsWithGivenTar.java
@@ -2,27 +2,38 @@ import java.util.* ;
 import java.io.*; 
 public class Solution 
 {
-    private static int f(int i, int tar, int arr[])
+    private static int f(int i, int tar, int arr[], int dp[][])
     {
-        int mod= Math.pow(10, 9) + 7;
-        if(tar == 0)
-            return 1;
-        if(i<0)
-            return 0;
+        int mod= (int)Math.pow(10, 9) + 7;
         
-        int take= f(i-1, tar-arr[i], arr)%mod;
-        int notTake= f(i-1, tar, arr)%mod;
-        return (take+notTake)%mod;
+		if(i == 0)
+		{
+			if(tar == 0 && arr[0]==0)
+				return 2;
+			if(tar == 0 || tar == arr[0])
+				return 1;
+			return 0;
+		}
+
+		if(dp[i][tar]!=-1)
+			return dp[i][tar];
+        
+        int take= (tar>=arr[i]) ? f(i-1, tar-arr[i], arr, dp)%mod : 0;
+        int notTake= f(i-1, tar, arr, dp)%mod;
+        return dp[i][tar]= (take+notTake)%mod;
     }
 
 	public static int countPartitions(int n, int d, int[] arr) 
     {
-        int dp[][]= new int[n][tar+1];
-        for(int k[]: dp)
+        int sum = Arrays.stream(arr).sum();
+        int tar = (sum - d) / 2;
+        if ( tar<0 || (sum - d) % 2 != 0)
+            return 0;
+
+        int dp[][] = new int[n][tar + 1];
+        for (int k[] : dp)
             Arrays.fill(k, -1);
-        
-        int sum= Arrays.stream(arr).sum();
-        int tar= (sum-d>0 && (sum-d)%2 == 0) ? (sum-d)/2 : 0;
-        return f(n-1, tar, arr);
+
+        return f(n - 1, tar, arr, dp);
 	}
 }
