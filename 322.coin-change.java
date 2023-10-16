383714ade8ab4015aa9b27b0c18495ca4a3434c5
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-17 03:18:02 +0530

    chhutta nahi hai

--- 322.coin-change.java
+++ 322.coin-change.java
@@ -7,33 +7,33 @@
 // @lc code=start
 class Solution 
 {
-    private int f(int i, int amount, int coins[], int dp[][]) 
+    private int f(int i, int tar, int coins[], int dp[][])
     {
         if(i == 0)
         {
-            if(amount%coins[i] == 0)
-                return amount/coins[0];
-            return (int) 1e9;
+            if(tar%coins[i] == 0)
+                return tar/coins[i];
+            return (int) 1e8;
         }
-        if(dp[i][amount] != -1)
-            return dp[i][amount];
+        if(dp[i][tar] != -1)
+            return dp[i][tar];
+        
+        int same= (tar>=coins[i]) ? 1 + f(i, tar-coins[i], coins, dp) :(int) 1e8;
+        int prev= f(i-1, tar, coins, dp);
 
-        int take= (amount>=coins[i]) ? 1 + f(i, amount-coins[i], coins, dp) : (int) 1e9;
-        int noTake= f(i-1, amount, coins, dp);
-
-        return dp[i][amount]= Math.min(take, noTake);
+        return dp[i][tar]= Math.min(same, prev);
     }
+
     public int coinChange(int[] coins, int amount) 
     {
         int dp[][]= new int[coins.length][amount+1];
         for(int r[]: dp)
             Arrays.fill(r, -1);
 
-        int tt= f(coins.length-1, amount, coins, dp);
-        if(tt >= 1e8)
+        int ans= f(coins.length-1, amount, coins, dp);
+        if(ans > (int) 1e6)
             return -1;
-        return tt;
+        else return ans;
     }
 }
 // @lc code=end
-
