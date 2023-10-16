ccf35d2d8fb401b568de2f74d05b2046d4a74d45
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-17 03:38:19 +0530

    target karta hai?!

--- 494.target-sum.java
+++ 494.target-sum.java
@@ -9,33 +9,37 @@
 // @lc code=start
 class Solution 
 {
-    int f(int i, int tar, int nums[], int dp[][])
+    private int f(int i, int tar, int arr[], int dp[][])
     {
-        if(i == 0)
-		{
-			if(tar == 0 && nums[0]==0)
-				return 2;
-			if(tar == 0 || tar == nums[0])
-				return 1;
-			return 0;
-		}
-        if(dp[i][tar] != -1)
-            return dp[i][tar];
-        
-        int take= (tar>=nums[i]) ? f(i-1, tar-nums[i], nums, dp) : 0;
-        int noTake= f(i-1, tar, nums, dp);
-        return dp[i][tar]= take + noTake;
+        if(i == 0) {
+            if(tar == 0 && arr[0] == 0)
+                return 2;
+            if(tar == 0 || arr[0] == tar)
+                return 1;
+            return 0;
+        }
+        if(tar < 0)
+            return 0;
+
+        int take= (tar>=arr[i]) ? f(i-1, tar-arr[i], arr, dp) : 0;
+        int noTake= f(i-1, tar, arr, dp);
+
+        return take+noTake;
     }
 
     public int findTargetSumWays(int[] nums, int target) 
     {
-        int tot= Arrays.stream(nums).sum();
-        if(tot-target < 0 || (tot-target)%2 == 1)
+        int tot= 0;
+        for(int ele: nums)
+            tot+= ele;
+        if(target > tot || (tot-target)%2!=0)
             return 0;
-        int tar= (tot - target)/2;
-
-        int dp[][]= new int[nums.length][tar+1];
-        return f(nums.length-1, tar, nums, dp);
+        
+        int s2= (tot-target)/2;
+        int dp[][]= new int[nums.length][s2+1];
+        for(int r[] : dp)
+            Arrays.fill(r, -1);
+        return f(nums.length-1, s2, nums, dp); 
     }
 }
 // @lc code=end
