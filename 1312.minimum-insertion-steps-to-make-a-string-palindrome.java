ad71936045c02c9ac014b64e52a014eae75fe20a
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-19 02:35:24 +0530

    pashmina

---
+++ 1312.minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1312 lang=java
+ *
+ * [1312] Minimum Insertion Steps to Make a String Palindrome
+ */
+
+// @lc code=start
+class Solution 
+{
+    public int minInsertions(String s) 
+    {
+        
+    }
+}
+// @lc code=end
+
