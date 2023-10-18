f6d7942e2c65743ec4ee5c2469c5cd1cb280393b
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-19 01:04:35 +0530

    Bijli se bhi tez bitch

--- 1143.longest-common-subsequence.java
+++ 1143.longest-common-subsequence.java
@@ -7,31 +7,30 @@
 // @lc code=start
 class Solution 
 {
-    private int f(int i, int j, char t1[], char t2[], int dp[][])
-    {
-        if(i == 0 || j == 0) {
-            return 0;
-        }
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        if(t1[i-1] == t2[j-1]) {
-            return dp[i][j]= 1 + f(i-1, j-1, t1, t2, dp);
-        }
-        else {
-            int oopar= f(i-1, j, t1, t2, dp);
-            int side= f(i, j-1, t1, t2, dp);
-            return dp[i][j]= Math.max(oopar, side);
-        }
-    }
     public int longestCommonSubsequence(String text1, String text2) 
     {
         int m= text1.length();
         int n= text2.length();
         int dp[][]= new int[m+1][n+1];
-        for(int r[]: dp)
-            Arrays.fill(r, -1);
+
+        char t1[]= text1.toCharArray();
+        char t2[]= text2.toCharArray();
         
-        return f(m, n, text1.toCharArray(), text2.toCharArray(), dp);
+        for(int i= 0; i<=m; i++)
+        {
+            for(int j= 0; j<=n; j++)
+            {
+                if(i==0 || j==0)
+                    dp[i][j]= 0;
+                
+                else if(t1[i-1] == t2[j-1])
+                    dp[i][j]= 1 + dp[i-1][j-1];
+                else
+                    dp[i][j]= Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[m][n];
     }
 }
 // @lc code=end
