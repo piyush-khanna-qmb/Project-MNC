ad4ced265db54e9c69046698c625acc225d57aec
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-19 03:20:13 +0530

    chakk de fatte

---
+++ dp30.java
@@ -0,0 +1,22 @@
+public class Solution 
+{
+    public static int canYouMake(String s1, String s2) 
+    {
+        int m= s.length(), n= t.length();
+        int dp[][]= new int[m+1][n+1];
+
+        for(int i= 1; i<=m; i++)
+        {
+            for(int j= 1; j<=n; j++)
+            {
+                if(s.charAt(i-1) == t.charAt(j-1))
+                    dp[i][j]= 1 + dp[i-1][j-1];
+                else
+                    dp[i][j]= Math.max(dp[i-1][j] , dp[i][j-1]);
+            }
+        }
+        int lcs= dp[m][n];
+
+        return m+n-(2*lcs);
+    }
+}
