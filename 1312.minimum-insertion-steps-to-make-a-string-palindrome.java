475b82a8e5c0a87d0b75f58bbde24186598484aa
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-19 02:49:21 +0530

    Teji pakad lo

--- 1312.minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312.minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -10,20 +10,22 @@ class Solution
     private int f(String s, String t)
     {
         int m= s.length(), n= t.length();
-        int dp[][]= new int[m+1][n+1];
+        int prev[]= new int[n+1];
+        int cur[]= new int[n+1];
 
         for(int i= 1; i<=m; i++)
         {
             for(int j= 1; j<=n; j++)
             {
                 if(s.charAt(i-1) == t.charAt(j-1))
-                    dp[i][j]= 1 + dp[i-1][j-1];
+                    cur[j]= 1 + prev[j-1];
                 else
-                    dp[i][j]= Math.max(dp[i-1][j] , dp[i][j-1]);
+                    cur[j]= Math.max(prev[j] , cur[j-1]);
             }
+            prev= Arrays.copyOf(cur, n+1);
         }
 
-        return dp[m][n];
+        return cur[n];
     }
 
     public int minInsertions(String s) 
