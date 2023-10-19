89d87bb0226f9ca79d6759c3665a5128c91bcdf3
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-20 05:11:15 +0530

    Dil vicho tere layi mai taim kadd ke

--- 115.distinct-subsequences.java
+++ 115.distinct-subsequences.java
@@ -7,35 +7,32 @@
 // @lc code=start
 class Solution 
 {
-    private int f(int i, int j, char s[], char t[], int dp[][])
+    public int numDistinct(String ss, String tt) 
     {
-        if(j==0)
-            return 1;
-        if(i==0)
-            return 0;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-
-        if(s[i-1] == t[j-1])
-        {
-            int yahi= f(i-1, j, s, t, dp);
-            int prev= f(i-1, j-1, s, t, dp);
-            return dp[i][j]= yahi+prev;
-        }
-        else
-            return dp[i][j]= f(i-1, j, s, t, dp);
-    }
-
-    public int numDistinct(String s, String t) 
-    {
-        int m= s.length(), n= t.length();
+        char s[]= ss.toCharArray();
+        char t[]= tt.toCharArray();
+        int m= ss.length(), n= tt.length();
         int dp[][]= new int[m+1][n+1];
-        for(int r[]: dp)
-            Arrays.fill(r, -1);
 
-        return f(m, n, s.toCharArray(), t.toCharArray(), dp);
+        for(int i= 0; i<=m; i++)
+            dp[i][0]= 1;
+            
+        for(int i= 1; i<=m; i++)
+        {
+            for(int j= 1; j<=n; j++)
+            {
+                if(s[i-1] == t[j-1])
+                {
+                    int yahi= dp[i-1][j];
+                    int prev= dp[i-1][j-1];
+                    dp[i][j]= yahi+prev;
+                }
+                else
+                    dp[i][j]= dp[i-1][j];
+            }
+        }
+
+        return dp[m][n];
     }
 }
 // @lc code=end
-
