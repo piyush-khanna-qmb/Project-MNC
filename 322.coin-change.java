0c71b8b66728921d805a1493e0e1e04341b70bc0
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-20 04:12:33 +0530

    Oye pappa ji

--- 322.coin-change.java
+++ 322.coin-change.java
@@ -13,15 +13,14 @@ class Solution
         {
             if(tar%coins[i] == 0)
                 return tar/coins[i];
-            return (int) 1e8;
+            return (int) 1e7;
         }
         if(dp[i][tar] != -1)
             return dp[i][tar];
         
-        int same= (tar>=coins[i]) ? 1 + f(i, tar-coins[i], coins, dp) :(int) 1e8;
+        int yahi= (tar>=coins[i]) ? 1 + f(i, tar-coins[i], coins, dp) : (int) 1e7;
         int prev= f(i-1, tar, coins, dp);
-
-        return dp[i][tar]= Math.min(same, prev);
+        return dp[i][tar]= Math.min(yahi, prev);
     }
 
     public int coinChange(int[] coins, int amount) 
@@ -29,11 +28,11 @@ class Solution
         int dp[][]= new int[coins.length][amount+1];
         for(int r[]: dp)
             Arrays.fill(r, -1);
-
+        
         int ans= f(coins.length-1, amount, coins, dp);
-        if(ans > (int) 1e6)
+        if(ans >= 1e7)
             return -1;
-        else return ans;
+        return ans;
     }
 }
 // @lc code=end
