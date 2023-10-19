5970f93292336d38095fe0d71af7b91c50cdfdf9
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-20 04:30:02 +0530

    Elvish bhai ke aage koi bol sakta hai kyaaa??

--- 1092.shortest-common-supersequence.java
+++ 1092.shortest-common-supersequence.java
@@ -7,58 +7,52 @@
 // @lc code=start
 class Solution 
 {
-    private String f(String s, String t)
+    public String shortestCommonSupersequence(String str1, String str2) 
     {
-        int m= s.length(), n= t.length();
+        int m= str1.length(), n= str2.length();
         int dp[][]= new int[m+1][n+1];
+
         for(int i= 1; i<=m; i++)
         {
-            for(int j= 1; j<=n; j++) 
+            for(int j= 1; j<=n; j++)
             {
-                if(s.charAt(i-1) == t.charAt(j-1))
+                if(str1.charAt(i-1) == str2.charAt(j-1))
                     dp[i][j]= 1 + dp[i-1][j-1];
                 else
-                    dp[i][j]= Math.max(dp[i][j-1] , dp[i-1][j]);
+                    dp[i][j]= Math.max(dp[i-1][j] , dp[i][j-1]);
             }
         }
-        int i= m, j= n;
+
         StringBuilder sb= new StringBuilder();
+        int i= m, j= n;
         while(i>0 && j>0)
         {
-            if(s.charAt(i-1) == t.charAt(j-1))
+            if(str1.charAt(i-1) == str2.charAt(j-1))
             {
-                sb.append(s.charAt(i-1));
+                sb.append(str1.charAt(i-1));
                 i--; j--;
             }
             else
             {
-                if(dp[i][j-1] >= dp[i-1][j])
-                {
-                    sb.append(t.charAt(j-1));
+                if(dp[i][j-1] >= dp[i-1][j]) {
+                    sb.append(str2.charAt(j-1));
                     j--;
                 }
-                else
-                {
-                    sb.append(s.charAt(i-1));
+                else {
+                    sb.append(str1.charAt(i-1));
                     i--;
                 }
             }
         }
-        while(i>0)
-        {
-            sb.append(s.charAt(i-1));
+        while(i>0) {
+            sb.append(str1.charAt(i-1));
             i--;
         }
-        while(j>0)
-        {
-            sb.append(t.charAt(j-1));
+        while(j>0) {
+            sb.append(str2.charAt(j-1));
             j--;
         }
         return sb.reverse().toString();
-    }
-    public String shortestCommonSupersequence(String str1, String str2) 
-    {
-        return f(str1, str2);
     }
 }
 // @lc code=end
