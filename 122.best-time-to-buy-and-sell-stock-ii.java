f01eaf07cbd684fe2c0b46f3a032ef8f7d754149
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-21 02:42:40 +0530

    scam karaalo mc se bas

--- 122.best-time-to-buy-and-sell-stock-ii.java
+++ 122.best-time-to-buy-and-sell-stock-ii.java
@@ -4,20 +4,36 @@
  * [122] Best Time to Buy and Sell Stock II
  */
 
-// For every valley that rises, there's a mountain of joy (profit) 😉
-
 // @lc code=start
 class Solution
 {
+    private int f(int i, int buy, int a[], int dp[][])
+    {
+        if(i == a.length)
+            return 0;
+        if(dp[i][buy] != -1)
+            return dp[i][buy];
+
+        if(buy == 1)
+        {
+            int yes= -a[i] + f(i+1, 0, a, dp);
+            int nah= f(i+1, 1, a, dp);
+            return dp[i][buy]= Math.max(yes, nah);
+        }
+        else
+        {
+            int yes= a[i] + f(i, 1, a, dp);
+            int nah= f(i+1, 0, a, dp);
+            return dp[i][buy]= Math.max(yes, nah);
+        }
+    }
     public int maxProfit(int[] a) 
     {
-        int last= a[0], pro= 0;
-        for(int ele: a) {
-            if(ele > last)  
-                pro+= (ele-last);
-            last= ele;
-        }
-        return pro;
+        int dp[][]= new int[a.length][2];
+        for(int r[]: dp)
+            Arrays.fill(r, -1);
+
+        return f(0, 1, a, dp);
     }
 }
 // @lc code=end
