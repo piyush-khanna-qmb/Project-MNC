092162a217c917730b4b8972b4dbb122eceb618b
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-21 02:22:17 +0530

    jay ho GPT

---
+++ 123.best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=123 lang=java
+ *
+ * [123] Best Time to Buy and Sell Stock III
+ */
+
+// @lc code=start
+class Solution 
+{
+    public int maxProfit(int[] prices) 
+    {
+        int firstBuy = Integer.MIN_VALUE;
+        int firstSell = 0;
+        int secondBuy = Integer.MIN_VALUE;
+        int secondSell = 0;
+
+        for (int price : prices) {
+            firstBuy = Math.max(firstBuy, -price);
+            firstSell = Math.max(firstSell, firstBuy + price);
+            secondBuy = Math.max(secondBuy, firstSell - price);
+            secondSell = Math.max(secondSell, secondBuy + price);
+        }
+
+        return secondSell;
+    }
+}
+// @lc code=end
+
