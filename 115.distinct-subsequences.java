e4062e4ebee8a790646371f8db4567be14eec70b
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-20 20:45:01 +0530

    jeh baat chhore

--- 115.distinct-subsequences.java
+++ 115.distinct-subsequences.java
@@ -7,32 +7,33 @@
 // @lc code=start
 class Solution 
 {
+    private int f(int i, int j, char s[], char t[], int dp[][])
+    {
+        if(j==0)
+            return 1;
+        if(i==0)
+            return 0;
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        if(s[i-1] == t[j-1])
+        {
+            int yahi= f(i-1, j, s, t, dp);
+            int agla= f(i-1, j-1, s, t, dp);
+            return dp[i][j]= yahi+agla;
+        }
+        else
+            return dp[i][j]= f(i-1, j, s, t, dp);
+    }
     public int numDistinct(String ss, String tt) 
     {
-        char s[]= ss.toCharArray();
-        char t[]= tt.toCharArray();
         int m= ss.length(), n= tt.length();
         int dp[][]= new int[m+1][n+1];
-
-        for(int i= 0; i<=m; i++)
-            dp[i][0]= 1;
-            
-        for(int i= 1; i<=m; i++)
-        {
-            for(int j= 1; j<=n; j++)
-            {
-                if(s[i-1] == t[j-1])
-                {
-                    int yahi= dp[i-1][j];
-                    int prev= dp[i-1][j-1];
-                    dp[i][j]= yahi+prev;
-                }
-                else
-                    dp[i][j]= dp[i-1][j];
-            }
-        }
-
-        return dp[m][n];
+        for(int r[]: dp)
+            Arrays.fill(r, -1);
+        
+        return f(m, n, ss.toCharArray(), tt.toCharArray(), dp);
     }
 }
 // @lc code=end
