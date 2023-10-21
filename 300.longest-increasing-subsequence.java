57361523ac1bd26298d4424dcb0c0a31b24eb223
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-22 02:57:20 +0530

    dreamum

--- 300.longest-increasing-subsequence.java
+++ 300.longest-increasing-subsequence.java
@@ -9,15 +9,15 @@ class Solution
 {
     private int f(int i, int j, int a[], int dp[][])
     {
-        if(i == a.length)
-            return 1;
+        if(i > a.length)
+            return 0;
         if(dp[i][j] != -1)
             return dp[i][j];
 
-        if(a[i] > a[j])
+        if(j == 0 || a[i-1] > a[j-1])
         {
             int take= 1 + f(i+1, i, a, dp);
-            int noTake= f(i+1, i, a, dp);
+            int noTake= f(i+1, j, a, dp);
             return dp[i][j]= Math.max(take, noTake);
         }
         return dp[i][j]= f(i+1, j, a, dp);
@@ -25,11 +25,10 @@ class Solution
     public int lengthOfLIS(int[] nums) 
     {
         int n= nums.length;
-        int dp[][]= new int[n][n];
+        int dp[][]= new int[n+1][n+1];
         for(int r[]: dp)
             Arrays.fill(r, -1);
-        return f(0, 0, nums, dp);
+        return f(1, 0, nums, dp);
     }
 }
 // @lc code=end
-
