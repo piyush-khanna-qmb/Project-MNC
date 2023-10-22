da42a1993afcdbd2b2a801abcdf6d0458f08479c
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-22 06:09:24 +0530

    sunny

--- 338.counting-bits.java
+++ 338.counting-bits.java
@@ -7,23 +7,13 @@
 // @lc code=start
 class Solution 
 {
-    private int numsOfOnes(String s)
-    {
-        int cnt= 0;
-        for(char c: s.toCharArray()) 
-        {
-            if(c=='1')
-                cnt++;
-        }
-        return cnt;
-    }
     public int[] countBits(int n) 
     {
         int ans[]= new int[n+1];
         for(int i= 0; i<=n; i++)
-            ans[i]= numsOfOnes(Integer.toBinaryString(i));
+            ans[i]= ans[i>>1] + (i&1);
+        
         return ans;
     }
 }
 // @lc code=end
-
