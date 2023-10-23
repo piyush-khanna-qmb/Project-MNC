597d09b445a09bf4fb9c2607bd2530d9b59f5f0b
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-24 05:04:02 +0530

    thudthudi apaahdam

--- 338.counting-bits.java
+++ 338.counting-bits.java
@@ -11,7 +11,7 @@ class Solution
     {
         int ans[]= new int[n+1];
         for(int i= 0; i<=n; i++)
-            ans[i]= ans[i>>1] + (i&1);
+            ans[i]= ans[i/2] + (i&1);
         
         return ans;
     }
