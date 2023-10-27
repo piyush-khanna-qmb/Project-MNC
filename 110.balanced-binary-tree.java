581c7b53942812c63cdd677b994d5406f0b6c8af
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-28 01:47:50 +0530

    Kalyanili!

--- 110.balanced-binary-tree.java
+++ 110.balanced-binary-tree.java
@@ -26,14 +26,18 @@ class Solution
     {
         if(root == null)
             return 0;
-        return 1 + Math.max(heightDedeBhainKL(root.left) , heightDedeBhainKL(root.right));
+        int lh= heightDedeBhainKL(root.left);
+        int rh= heightDedeBhainKL(root.right);
+        if(Math.abs(lh-rh) > 1  || lh == -1 || rh == -1)
+            return -1;
+        return 1 + Math.max(lh, rh);
     }
     
     public boolean isBalanced(TreeNode root) 
     {
         if(root == null)
             return true;
-        return heightDedeBhainKL(root.left) - heightDedeBhainKL(root.right) <= 1;
+        return heightDedeBhainKL(root) != -1;
     }
 }
 // @lc code=end
