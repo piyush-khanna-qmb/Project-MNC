7fc98944f34c4005e23c945065b2d02656a5b80e
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-29 03:00:18 +0530

    badam doodh

---
+++ 101.symmetric-tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution 
+{
+    private boolean checkKariyo(TreeNode s, TreeNode t)
+    {
+        if(s == null || t == null)
+            return s==t;
+        
+        return s.val==t.val && checkKariyo(s.left, t.right) && checkKariyo(s.right, t.left);
+    }
+
+    public boolean isSymmetric(TreeNode root) 
+    {
+        if(root == null)
+            return true;
+        
+        return checkKariyo(root, root);
+        
+    }
+}
