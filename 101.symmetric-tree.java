22e934e84a9c51da8efdd3492dd482de5ea05e5b
piyush-khanna-vb <piyushkhannavb@gmail.com>
2023-10-30 22:03:24 +0530

    streak

--- 101.symmetric-tree.java
+++ 101.symmetric-tree.java
@@ -22,7 +22,6 @@ class Solution
         
         return s.val==t.val && checkKariyo(s.left, t.right) && checkKariyo(s.right, t.left);
     }
-
     public boolean isSymmetric(TreeNode root) 
     {
         if(root == null)
