010a8259a31123620c0513240a0ad9555e556c6f
piyush-khanna-qmb <piyushkhannavb@gmail.com>
2023-11-05 00:05:56 +0530

    Main Hoon Don

---
+++ 2236.root-equals-sum-of-children.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=2236 lang=java
+ *
+ * [2236] Root Equals Sum of Children
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution 
+{
+    public boolean checkTree(TreeNode root) 
+    {
+        return root.val == (root.left.val+root.right.val);
+    }
+}
+// @lc code=end
+
