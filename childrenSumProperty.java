3cad04e54b80f629305427e9088fac1d88edba43
piyush-khanna-qmb <piyushkhannavb@gmail.com>
2023-11-08 00:47:00 +0530

    bhasadhbaaji

---
+++ childrenSumProperty.java
@@ -0,0 +1,25 @@
+import java.util.* ;
+import java.io.*; 
+/*************************************************************
+
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode < Integer > {
+        int data;
+        BinaryTreeNode < Integer > left;
+        BinaryTreeNode < Integer > right;
+
+    public BinaryTreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+*************************************************************/
+
+public class Solution 
+{
+    public static void changeTree(BinaryTreeNode < Integer > root) 
+    {
+        
+    }
+}
