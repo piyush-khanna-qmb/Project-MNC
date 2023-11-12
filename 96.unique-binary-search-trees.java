fb2762ef152d03db009966e264db6f6d8ac16951
piyush-khanna-qmb <piyushkhannavb@gmail.com>
2023-11-12 23:24:19 +0530

    Happy Diwali

---
+++ 96.unique-binary-search-trees.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=96 lang=java
+ *
+ * [96] Unique Binary Search Trees
+ */
+
+// @lc code=start
+class Solution {
+    public int numTrees(int n) {
+        
+    }
+}
+// @lc code=end
+
