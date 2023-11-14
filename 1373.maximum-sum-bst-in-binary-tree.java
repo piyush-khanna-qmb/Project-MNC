2005f3305afe79fd53c6b59e1d1eb76bbde7d0bc
piyush-khanna-qmb <piyushkhannavb@gmail.com>
2023-11-15 04:23:10 +0530

    internet ne aukat dikha di

--- 1373.maximum-sum-bst-in-binary-tree.java
+++ 1373.maximum-sum-bst-in-binary-tree.java
@@ -20,11 +20,43 @@
  *     }
  * }
  */
-class Solution 
-{
-    public int maxSumBST(TreeNode root) 
-    {
-        
+public class Solution {
+    int maxSum = 0;
+
+    public int maxSumBST(TreeNode root) {
+        validateBST(root);
+        return maxSum;
+    }
+
+    private Result validateBST(TreeNode node) {
+        if (node == null) {
+            return new Result(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+
+        Result left = validateBST(node.left);
+        Result right = validateBST(node.right);
+
+        if (left.isValid && right.isValid && node.val > left.max && node.val < right.min) {
+            int sum = node.val + left.sum + right.sum;
+            maxSum = Math.max(maxSum, sum);
+            return new Result(true, sum, Math.min(node.val, left.min), Math.max(node.val, right.max));
+        } else {
+            return new Result(false, 0, 0, 0);
+        }
+    }
+
+    private static class Result {
+        boolean isValid;
+        int sum;
+        int min;
+        int max;
+
+        Result(boolean isValid, int sum, int min, int max) {
+            this.isValid = isValid;
+            this.sum = sum;
+            this.min = min;
+            this.max = max;
+        }
     }
 }
 // @lc code=end
