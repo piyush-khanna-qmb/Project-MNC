b37f343ca6b39915a036638f2cf0ca6fb89e6d84
piyush-khanna-qmb <piyushkhannavb@gmail.com>
2023-11-30 20:40:27 +0530

    ding ding

---
+++ 547.number-of-provinces.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=547 lang=java
+ *
+ * [547] Number of Provinces
+ */
+
+// @lc code=start
+class Solution 
+{
+    List<List<Integer>> getAdjList(int[][] isConnected)
+    {
+        List<List<Integer>> adj= new ArrayList();
+        
+    }
+    public int findCircleNum(int[][] isConnected) 
+    {
+        
+    }
+}
+// @lc code=end
+
