5c54a6cdba95542125380bf7097fad2adfe8d716
piyush-khanna-qmb <piyushkhannavb@gmail.com>
2023-12-02 02:38:46 +0530

    kaali teri gutt

--- 547.number-of-provinces.java
+++ 547.number-of-provinces.java
@@ -10,10 +10,43 @@ class Solution
     List<List<Integer>> getAdjList(int[][] isConnected)
     {
         List<List<Integer>> adj= new ArrayList();
+        for(int i= 0; i<isConnected.length; i++)
+            adj.add(new ArrayList());
+        
+        for(int i= 0; i<isConnected.length; i++)
+        {
+            for(int j= 0; j<isConnected[0].length; j++)
+            {
+                if(i!=j && isConnected[i][j] == 1)
+                    adj.get(i).add(j);
+            }
+        }
     }
     public int findCircleNum(int[][] isConnected) 
     {
+        if(isConnected.length <= 1)
+            return isConnected.length;
+
+        List<List<Integer>> adj= getAdjList(isConnected);
+        Queue<Integer> q= new LinkedList();
+        int c= 0;
+        boolean vis[]= new boolean[isConnected.length];
+        for(int i= 0; i<isConnected.length; i++)
+        {
+            if(!vis[i])
+            {
+                c++;
+                q.offer(i);
+                while(!q.isEmpty())
+                {
+                    int yeWala= q.poll();
+                    for(int ele: adj.get(i))
+                        q.offer(ele);
+                }
+            }
+        }
         
+        return c;
     }
 }
 // @lc code=end
