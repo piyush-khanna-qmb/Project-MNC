ddf7caa9027eaa22728b3ece9dff0374d542b650
piyush-khanna-qmb <piyushkhannavb@gmail.com>
2023-12-15 23:04:05 +0530

    epic chillstep

--- quickSort.java
+++ quickSort.java
@@ -1,31 +1,41 @@
 import java.util.*;
-class quickSort {
-    private static int partition(int arr[], int low, int high) {
-        int p= arr[low], i= low, j= high, tmp;
-        while(i<j) {
-            while(i<high && arr[i]<=p)
+class quickSort 
+{        
+    private static void qS(int arr[], int st, int end)
+    {
+        if(st < end)
+        {
+            int p= pivot(arr, st, end);
+            qS(arr, st, p-1);
+            qS(arr, p+1, end);
+        }
+    }
+
+    private static int pivot(int arr[], int st, int end)
+    {
+        int p= arr[st], i= st, j= end;
+        while(i<j)
+        {
+            while(i<end && arr[i]<=p)
                 i++;
-            while(j>low && arr[j]>p)
+            while(j>st && arr[j]>p)
                 j--;
+            
             if(i<j) {
-                tmp= arr[i];
+                int tmp= arr[i];
                 arr[i]= arr[j];
                 arr[j]= tmp;
             }
         }
-        tmp= arr[low];
-        arr[low]= arr[j];
+        int tmp= arr[st];
+        arr[st]= arr[j];
         arr[j]= tmp;
+
         return j;
     }
-    private static void qS(int arr[], int low, int high) {
-        if(low>high)
-            return;
-        int part= partition(arr, low, high);
-        qS(arr, low, part-1);
-        qS(arr, part+1, high);
-    }    
-    public static void main(String args[]) {
+
+    public static void main(String args[]) 
+    {
         int a[]= {12,65,3,7,98,1,65,8,3,2};
         qS(a, 0, a.length-1);
         System.out.println(Arrays.toString(a));
