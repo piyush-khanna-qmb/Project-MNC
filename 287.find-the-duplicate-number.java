5c541c5a03994ffb4716ae220f65f7c58ff052f5
piyush-khanna-qmb <piyushkhannavb@gmail.com>
2023-12-17 00:18:51 +0530

    solid body

--- 287.find-the-duplicate-number.java
+++ 287.find-the-duplicate-number.java
@@ -5,9 +5,18 @@
  */
 
 // @lc code=start
-class Solution {
-    public int findDuplicate(int[] nums) {
-        
+class Solution 
+{
+    public int findDuplicate(int[] nums) 
+    {
+        int ans= 1;
+        for(int i= 0; i<nums.length; i++)
+        {
+            int k= nums[i];
+            ans= ans^nums[i];
+            if(ans == 0)
+                return k;
+        }        
     }
 }
 // @lc code=end
