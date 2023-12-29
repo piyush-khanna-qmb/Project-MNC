c2364991397c3d0089a623691aa86e0f0ec01597
piyush-khanna-qmb <piyushkhannavb@gmail.com>
2023-12-29 18:18:14 +0530

    huihui

---
+++ 542.01-matrix.java
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=542 lang=java
+ *
+ * [542] 01 Matrix
+ */
+
+// @lc code=start
+class Solution 
+{
+    public int[][] updateMatrix(int[][] mat) 
+    {
+        int ans[][]
+    }
+}
+// @lc code=end
+
