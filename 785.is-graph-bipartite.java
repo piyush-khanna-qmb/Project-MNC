c8476625b80b7aadbf0d7208248e78aaf9858926
piyush-khanna-qmb <piyushkhannavb@gmail.com>
2024-01-02 01:38:30 +0530

    tu mile

---
+++ 785.is-graph-bipartite.java
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=785 lang=java
+ *
+ * [785] Is Graph Bipartite?
+ */
+
+// @lc code=start
+class Solution 
+{
+    public boolean isBipartite(int[][] graph)
+    {
+        
+    }
+}
+// @lc code=end
+
